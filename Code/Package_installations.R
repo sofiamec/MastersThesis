ee051a2c1e21d257d715387b495d5b9fe571c250
Martina <46865458+martinahb@users.noreply.github.com>
2020-01-31 09:51:37 +0100

    Create Package_installations.R
    Adding package installations required for exploring data

---
+++ Code/Package_installations.R
@@ -0,0 +1,12 @@
+
+# for plotting
+install.packages("ggplot2")
+
+# for reshaping the dataset to works for ggplot
+install.packages("reshape2")
+
+# for visualisation in nice colours
+install.packages("RColorBrewer")
+
+# for printing tables compatible with LaTeX
+install.packages("xtable")
