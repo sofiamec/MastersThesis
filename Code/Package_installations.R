401371f98525778bcae179bbbb71d9f85c64dfef
sofiamec <sofiamec@users.noreply.github.com>
2020-02-12 16:41:48 +0100

    Remove edgeR-packages

--- Code/Package_installations.R
+++ Code/Package_installations.R
@@ -19,13 +19,6 @@ install.packages("tidyverse")
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("DESeq2")
-# for analysing the datasets and identify DAGs
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
-#BiocManager::install("edgeR")
-
-# for calculating overdispersion in edgeR
-#install.packages("statmod")
 
 # For computing AUCs with trapeziodal rule
 install.packages("pracma")
