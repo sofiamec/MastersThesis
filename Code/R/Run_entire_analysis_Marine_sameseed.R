0aba4a946074cef345c9075c81b9db392549689a
Martina <46865458+martinahb@users.noreply.github.com>
2020-02-27 10:15:54 +0100

    Update Run_entire_analysis_Marine_sameseed.R

--- Code/R/Run_entire_analysis_Marine_sameseed.R
+++ Code/R/Run_entire_analysis_Marine_sameseed.R
@@ -16,7 +16,7 @@ library(pracma)
 #                            (the rest of the code should adjust)
 #===================================================================================================================================
 ## Selecting parameters and data:
-onTerra = F                                                 # use T if running analysis on Terra (large scale settings applied)
+onTerra = T                                                # use T if running analysis on Terra (large scale settings applied)
 saveName = "Marine"     # "Gut2" or "Marine"                  # this will in turn load the correct data
 f = 0.10                                                    # Desired total fraction of genes to be downsampled. It will not be exact. The effects will be balanced
 numberOfStrata = 3                                          # sets the number of groups for dividing gene abundance and variability
