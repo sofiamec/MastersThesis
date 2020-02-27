50aed7827a6bcefb5b61c7d485a5bf4f0a741b40
sofiamec <sofiamec@users.noreply.github.com>
2020-02-27 17:14:21 +0100

    Troubleshooting
    Copied the old troubleshooting to a separate script and continued working with the "new".

--- Code/R/Troubleshooting.R
+++ Code/R/Troubleshooting.R
@@ -1,13 +1,14 @@
 # Troubleshooting high sequencing depths 
+library(reshape2)
 
-#=================================== Choose dataset to troubleshoot ===========================================
+#=================================== Choose "good" dataset with low sequencing depth ========================
 saveName = "Marine" # Choose dataset. Ex: "Gut2" or "Marine"
-m = 50              # Number of samples in each group (total nr samples = 2*m)
-d = 10000000        # Desired sequencing depth per sample. It will not be exct
-dD="10M"            # display name
-q = 1.5   #1.5, 2.5          # Fold-change for downsampling
+m = 10              # Number of samples in each group (total nr samples = 2*m)
+d = 10000           # Desired sequencing depth per sample. It will not be exct
+dD="10k"            # display name
+q = 1.5             # Fold-change for downsampling
 f = 0.10            # Desired total fraction of genes to be downsampled. It will not be exact. The effects will be balanced
-run=10    #7,10           # choose wich run to extract
+run=1               # choose wich run to extract
 
 
 { # Quickly gives the case the correct name
@@ -26,72 +27,143 @@ run=10    #7,10           # choose wich run to extract
 }
 
 # Read dataset 
-DownSampledData=read.csv(sprintf("../../Intermediate/test_qvalues/%s/%s/DownSampledData_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
-DAGs=read.csv(sprintf("../../Intermediate/test_qvalues/%s/%s/DAGs_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
-
-# =================================== Source Analysis of DAGs ==================================================
+DownSampledData=read.csv(sprintf("../../Intermediate/%s/%s/DownSampledData_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
+DAGs=read.csv(sprintf("../../Intermediate/%s/%s/DAGs_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
 
 source("Analysis_of_DAGs_separate.R")
 
-#======================================Plot TP and FP in significance-order ====================================
+# Save output named after this dataset
+DataLow=DownSampledData
+DAGsLow=DAGs
+ResDESeqLow=ResDESeq
+dDLow=dD
 
-PR=c()
-for (i in 1:nrow(DownSampledData)) {
-  
-  if (BinTP[i]==0){
-    PR[i]="FP"
+#=================================== Choose "bad" dataset with high sequencing depth ========================
+saveName = "Marine" # Choose dataset. Ex: "Gut2" or "Marine"
+m = 10              # Number of samples in each group (total nr samples = 2*m)
+d = 1000000         # Desired sequencing depth per sample. It will not be exct
+dD="1M"             # display name
+q = 1.5             # Fold-change for downsampling
+f = 0.10            # Desired total fraction of genes to be downsampled. It will not be exact. The effects will be balanced
+run=1               # choose wich run to extract
+
+
+{ # Quickly gives the case the correct name
+  if (saveName == "Gut2"){
+    plotName = "Human Gut II"
+  } else if (saveName == "Marine"){
+    plotName = "Marine"
   } else {
-    PR[i]="TP"
+    sprintf("Missing name for dataset")
   }
-}
-x=seq(1,nrow(DownSampledData), by=1)
-y=c(rep(1, nrow(DownSampledData)))
-HeatmapPR=data.frame(x,y, PR)
-
-ggplot(HeatmapPR, aes(x=x, y=y, fill=PR)) +
-  geom_tile(aes(fill = PR)) +
-  #scale_fill_manual(values = c("red", "blue")) + 
-  scale_fill_viridis_d(begin = 0, end = 1, alpha = 0.5) +  
-  labs(title="TP and FP in significance order", x = "Genes (from most to least significant)" , y = "  ") #, fill = "AUC-values") 
-
-#======================================= Dispersion ============================================================
-# Look att dispersion
-summary(ResDESeq[,4])
-
-
-#=================================Summary of log2 fold change for TP and FP =====================================
-# check summary for TP and FP
-
-# log2 fold change <0, TP 
-ResultLTrue=ResDESeq[rownames(ResDESeq) %in% rownames(DAGs) & ResDESeq[,3]<0 & ResDESeq[,2]<0.05 ,]
-summary(ResultLTrue[,3])
-
-# log2 fold change <0, FP  
-ResultLFalse=ResDESeq[!rownames(ResDESeq) %in% rownames(DAGs) & ResDESeq[,3]<0 & ResDESeq[,2]<0.05 ,]
-summary(ResultLFalse[,3])
-
-# log2 fold change >0, TP  
-ResultBTrue=ResDESeq[rownames(ResDESeq) %in% rownames(DAGs) & ResDESeq[,3]>0 & ResDESeq[,2]<0.05 ,]
-summary(ResultBTrue[,3])
-
-# log2 fold change >0, FP  
-ResultBFalse=ResDESeq[!rownames(ResDESeq) %in% rownames(DAGs) & ResDESeq[,3]>0 & ResDESeq[,2]<0.05 ,]
-summary(ResultBFalse[,3])
-
-
-# ============================================= Check for zeros ================================================= 
-# Total number of zeros in the dataset
-sum(DownSampledData==0)
-
-# create vector containg the number of zeroes per gene
-zerosPerGene=c()
-for (i in 1:nrow(DownSampledData)) {
-  zerosPerGene[i]=sum(DownSampledData[i,]==0)
+  
+  # Names for a certain dataset and name    # Results in:
+  saveExpDesign = sprintf("m%d_d%s_10q%d_f%d", m, dD, q*10, f*100)
+  plotExpDesign = sprintf("m=%d, d=%s, q=%g, f=%d%%",m,dD,q,f*100)
+  
 }
 
-summary(zerosPerGene)
+# Read dataset 
+DownSampledData=read.csv(sprintf("../../Intermediate/%s/%s/DownSampledData_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
+DAGs=read.csv(sprintf("../../Intermediate/%s/%s/DAGs_run%d.csv", saveName, saveExpDesign, run), header = T, row.names = 1)
 
-# See if the gene with maximum zeros is among the introduced DAGs (if yes, returns at what index)
-match(row.names(DownSampledData[match(92, zerosPerGene),]), rownames(DAGs))
+source("Analysis_of_DAGs_separate.R")
+
+# Save output named after this dataset
+DataHigh=DownSampledData
+DAGsHigh=DAGs
+ResDESeqHigh=ResDESeq
+dDHigh=dD
+
+# Remove non used ouptus
+rm(DownSampledData, DAGs, ResDESeq, AUCs, deseqROCAUC, meanROCs, ROCs)
+
+#========================= Dispertion and log2 fold change plots ===========================================
+
+# check that the same DAGs have been introduced 
+all(rownames(DAGsLow)==rownames(DAGsHigh))
+
+# vector inicating if each gene is a DAG or not (DAG-indicator)
+DAG=c()
+for (i in 1:nrow(ResultLog2Disp)) {
+  if (rownames(ResultLog2Disp[i,]) %in% rownames(DAGsLow)) {
+    DAG[i]=1
+  } else {DAG[i]=0}
+}
+
+# dataframe with log2 and disp for both datasets, and DAG-indicator
+ResultLog2Disp=data.frame(ResDESeqLow[order(rownames(ResDESeqLow)),c(3,4)], ResDESeqHigh[order(rownames(ResDESeqHigh)),c(3,4)], DAG)
+colnames(ResultLog2Disp)<- c("log2Low", "DispLow", "log2High", "DispHigh", "DAG")
+
+# Dispertion plot
+ggplot(ResultLog2Disp, aes(x=DispHigh, y=DispLow)) + 
+  scale_color_viridis_d(begin = 0, end = 0.5, name=" ", labels=c("No DAG","DAG")) +
+  geom_point(aes(color=factor(DAG)), size=1) +
+  labs(title="Dispersion for same dataset with different seq. depths", 
+       x=sprintf("Sequencing depth %s", dDHigh), y=sprintf("Sequencing depth %s", dDLow))
+
+# log2 plot
+ggplot(ResultLog2Disp, aes(x=log2High, y=log2Low)) +
+  scale_color_viridis_d(begin = 0, end = 0.5, name=" ", labels=c("No DAG","DAG")) +
+  geom_point(aes(color=factor(DAG)), size=1) +
+  labs(title="log2 fold change for same dataset with different seq. depths", 
+       x=sprintf("Sequencing depth %s", dDHigh), y=sprintf("Sequencing depth %s", dDLow))
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Fulplottar 
+#ResultLow=ResDESeqLow[order(rownames(ResDESeqLow)),]
+#ResultHigh=ResDESeqHigh[order(rownames(ResDESeqHigh)),]
+#plot(ResultHigh[,4], ResultLow[,4])
+#plot(ResultHigh[,3], ResultLow[,3])
+
+# NOT NEEDED 
+#=============================== Prepare datasets for gg-plot ==============================================
+
+# vector with rownames, ordered alphabetically/numerically
+#Genes=rownames(ResDESeqLow[order(rownames(ResDESeqLow)),])  
+
+# extract log2 for both low and high dataset
+#ResultLog2=data.frame(Genes, ResDESeqLow[order(rownames(ResDESeqLow)),3], ResDESeqHigh[order(rownames(ResDESeqHigh)),3])
+#colnames(ResultLog2)<- c("Gene", "Low", "High")
+
+# extract dispertion for both low and high dataset
+#ResultDisp=data.frame(Genes, ResDESeqLow[order(rownames(ResDESeqLow)),4], ResDESeqHigh[order(rownames(ResDESeqHigh)),4])
+#colnames(ResultDisp)<- c("Gene", "Low", "High")
+
+# melt both datasets and merge them 
+#ResultLog2Melt=melt(ResultLog2, id="Gene", value.name = "log2", variable.name = "Type")
+#ResultDispMelt=melt(ResultDisp, id="Gene", value.name = "Dispertion", variable.name = "Type")
+#ResultGGFormat=merge(ResultLog2Melt,ResultDispMelt)
+
+# remove intermediate datasets
+#rm(Genes, ResultLog2, ResultDisp, ResultLog2Melt, ResultDispMelt)
+
 
 
