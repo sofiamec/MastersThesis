6dbc3311e0f026507b34c677d3e87cf292068b6b
Martina Billstein <marbil@student.chalmers.se>
2020-04-27 10:09:54 +0200

    Rearranging old scripts into new structure

---
+++ Code/R/Final_script_Gut2.R
@@ -0,0 +1,23 @@
+## Selecting parameters and data:
+onTerra = T                                                 # use T if running analysis on Terra (large scale settings applied)
+saveName = "Gut2"  # "Gut2", "Marine" or "Resistance      # this will in turn load the correct data
+f = 0.10                                                    # Desired total fraction of genes to be downsampled. It will not be exact. The effects will be balanced
+runStrata = F
+extraDesigns = F                                              # use T if the analysis of DAGs should be performed with DESeq2. Use F to choose OGLM instead
+analyses <- c("DESeq", "OGLM")
+
+source("Functions_and_libraries.R")
+
+#########################################
+
+
+suppressWarnings(source("Setup_designs_final.R"))
+  
+loadData = F  
+for (analysis in analyses){
+  cat(sprintf("==================== %s_%s ====================\n", saveName, analysis), file = stdout()) 
+  source("Run_entire_analysis_final.R")
+  loadData = T  
+}
+  
+rm(relations, boldvalue2, AllPlotDesigns, plotExpDesign, saveExpDesign, repeats, extraL)
