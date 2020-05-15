016d0a30a3592fce05d03bffc0a9277e94599eee
Martina Billstein <marbil@student.chalmers.se>
2020-05-15 09:13:37 +0200

    Run for trade-off plots

--- Code/R/Final_script_Marine.R
+++ Code/R/Final_script_Marine.R
@@ -4,7 +4,7 @@ saveName = "Marine"  # "Gut2", "Marine" or "Resistance      # this will in turn 
 f = 0.10                                                    # Desired total fraction of genes to be downsampled. It will not be exact. The effects will be balanced
 runStrata = T
 extraDesigns = T                                              # use T if the analysis of DAGs should be performed with DESeq2. Use F to choose OGLM instead
-analyses <- c("DESeq", "OGLM")
+analyses <- c("DESeq")#, "OGLM")
 
 source("Functions_and_libraries.R")
 
@@ -13,7 +13,7 @@ source("Functions_and_libraries.R")
 
 suppressWarnings(source("Setup_designs_final.R"))
   
-loadData = F  
+loadData = T # CHANGE THIS TO "T" IN THE FINAL VERSION!!  
 for (analysis in analyses){
   cat(sprintf("==================== %s_%s ====================\n", saveName, analysis), file = stdout()) 
   source("Run_entire_analysis_final.R")
