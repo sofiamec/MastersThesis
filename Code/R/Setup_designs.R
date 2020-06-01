035918059344a3b483467c32be08b4878b5c8aea
Martina Billstein <marbil@student.chalmers.se>
2020-06-01 09:39:09 +0200

    Update Setup of designs

--- Code/R/Setup_designs.R
+++ Code/R/Setup_designs.R
@@ -1,48 +1,38 @@
-#===================================================================================================================================
-# SET-UP SECTION
-#===================================================================================================================================
-
-# Setup script for assigning correct names for a given design as well as preprocessing of datasets
-# This script has to be run in combination with the "Run_entire_analysis"-script
+# This setup script assigns correct names for a given design as well as preprocesses the selected dataset
 
 ####################################################################################################################################
-#===================================================================================================================================
-#                      Only change these parameters for different results! 
-#                            (the rest of the code should adjust)
-#===================================================================================================================================
-# Test-settings (Remove this in final script!)
-if (onTerra==F){
-  repeats = 2                                               # sets the number of runs for each case (experimental design and q)
-  limitNA = 0                                                   # the lowest amount of observaitons needed to produce a results other than NA
-  savePlot = T                                              # use T when plots should be saved (for many repeats)
-  #loadData = F                                              # use T if it is a rerun of existing results
-  effectsizes=c(1.5)#,3)                                             # q = Fold-change for downsampling
-  groupSize<-c(3)#,5)#,10,30)#,30,50)                                            # m = Number of samples in each group (total nr samples = 2*m)
-  sequencingDepth<-c(10000)#, 20000)#,100000)#, 5000000)#,10000,1000000,5000000,10000000)      # d = Desired sequencing depth per sample
-  sequencingDepthName<-c("10 k")#, "20 k")#, "100 k")#, "500 k")# "10 k","500 k","1 M", "5 M", "10 M")         # dD = Displayed names for sequencing depths
-  sequencingSaveName<-c("10k")#, "20 k")#, "100 k")#, "500 k")# "10 k","500 k","1 M", "5 M", "10 M")         # dD = Displayed names for sequencing depths
-}
 
-# Real settings
+# Real settings (Full-scale setup)
 if (onTerra==T){
   repeats = 100                                              # sets the number of runs for each case (experimental design and q)
   limitNA = 10                                                   # the lowest amount of observaitons needed to produce a results other than NA
   savePlot = T                                              # use T when plots should be saved (for many repeats)
-  loadData = F                                              # use T if it is a rerun of existing results
   
-  # flytta eventuellt allt detta till setup-skriptet
   if (saveName =="Resistance"){
     effectsizes=c(5,10)                                     # q = Fold-change for downsampling
   } else {
-    effectsizes=c(1.5,3)                                   # q = Fold-change for downsampling
+    effectsizes=c(1.5,3)                                    # q = Fold-change for downsampling
   }
   
   groupSize<-c(3,5,10,30,50)                                # m = Number of samples in each group (total nr samples = 2*m)
   # sequencing depths are set later depending on dataset    # d and dD = Desired sequencing depths and how it should be displayed
 }
 
-# Extra-settings
-extraL=0                                                    # unless extraDesigns are added, the length of added designs is 0                               
+# Test-settings 
+if (onTerra==F){
+  repeats = 2                                                      # sets the number of runs for each case (experimental design and q)
+  limitNA = 0                                                      # the lowest amount of observaitons needed to produce a results other than NA
+  savePlot = F                                                     # use T when plots should be saved (for many repeats)
+  effectsizes=c(1.5)#,3)                                           # q = Fold-change for downsampling
+  groupSize<-c(3)#,5)#,10,30)#,30,50)                                                                       # m = Number of samples in each group (total nr samples = 2*m)
+  sequencingDepth<-c(10000)#, 20000)#,100000)#, 5000000)#,10000,1000000,5000000,10000000)                   # d = Desired sequencing depth per sample
+  sequencingDepthName<-c("10 k")#, "20 k")#, "100 k")#, "500 k")# "10 k","500 k","1 M", "5 M", "10 M")      # dD = Displayed names for sequencing depths
+  sequencingSaveName<-c("10k")#, "20 k")#, "100 k")#, "500 k")# "10 k","500 k","1 M", "5 M", "10 M")        # dD = Displayed names for sequencing depths
+}
+
+
+# Extra-settings (Used for analysing trade-off)
+extraL=0                                                    # Unless extraDesigns are added, the length of added designs is 0                               
 if (extraDesigns==T){
   # The 3 following must have equal lengths!                # Combined they give more results for trade-off curves. Here with m*d = 3M, 3M and 5M respectively
   extraSeqDepth=c(200000,500000,250000)      
@@ -53,11 +43,10 @@ if (extraDesigns==T){
 }
 # Strata-settings
 if (runStrata==T){
-  numberOfStrata = 3                                        # sets the number of groups for dividing gene abundance and variability
-  strataClass<-factor(c("low","medium","high"), levels=c("low","medium","high")) #strataClass<-c("low","medium", "high")                      # should correspond to the number of stratas
+  numberOfStrata = 3                                                                # Sets the number of groups for dividing gene abundance and variability
+  strataClass<-factor(c("low","medium","high"), levels=c("low","medium","high"))    # Should correspond to the number of stratas
 }
-#===================================================================================================================================
-#===================================================================================================================================
+
 ####################################################################################################################################
 
 ## Loading original data
@@ -68,10 +57,9 @@ if(saveName == "Gut2"){
   plotName = "Human Gut II"
   Gut2Original <- read.table("../../Data/Raw_data/HumanGutII_COGcountsRaw.txt", header=T, row.names = 1)
   Gut2Intermediate = Gut2Original[,colSums(Gut2Original)>=5000000]   # Filter out samples with sequencing depth below the maximum sequencing depth of the experimental design
-  Gut2 <- remove_low_counts(Gut2Intermediate)
+  Gut2 <- remove_low_counts(Gut2Intermediate)                        # Fiters out genes with low counts
   Data = Gut2
   
-  boldvalue2="0"
   relations<-c(3000000, 5000000)
   if (onTerra==T){
     sequencingDepth<-c(10000,100000,500000,1000000,5000000)
@@ -85,10 +73,9 @@ if(saveName == "Gut2"){
   plotName = "Marine"
   MarineOriginal <- read.table("../../Data/Raw_data/Marine_COGcountsRaw.txt", header=T, row.names = 1)
   MarineIntermediate = MarineOriginal[,colSums(MarineOriginal)>=10000000]   # Filter out samples with sequencing depth below the maximum sequencing depth of the experimental design
-  Marine <- remove_low_counts(MarineIntermediate)
+  Marine <- remove_low_counts(MarineIntermediate)                           # Fiters out genes with low counts
   Data = Marine
-  
-  boldvalue2="5e+07"                                              
+                                              
   relations<-c(3000000,5000000)
   if (onTerra==T){
     sequencingDepth<-c(10000,100000,500000,1000000,5000000,10000000)
@@ -108,16 +95,15 @@ if(saveName == "Gut2"){
   ResistanceOriginal<-ResistanceOriginal[-2,]
   ResistanceOriginal=data.frame(row.names = row.names(ResistanceOriginal)[-1], apply(ResistanceOriginal[-1,],2,as.integer))
   ResistanceIntermediate=ResistanceOriginal[,colSums(ResistanceOriginal)>=10000000] # Filter out samples with sequencing depth below the maximum sequencing depth of the experimental design
-  Resistance <- remove_low_counts(ResistanceIntermediate) # This is the dataset used in analysis where samples and genes with low counts are removed
+  Resistance <- remove_low_counts(ResistanceIntermediate)                           # Fiters out genes with low counts
   
   Data = Resistance
     
-  runStrata=F
-  boldvalue2="5e+07"                                              
+  runStrata=F # The strata analysis cannot be run for this dataset due to too few genes                                          
   relations<-c(3000000,5000000)
   if (onTerra==T){
-    sequencingDepth<-c(10000,100000,500000,1000000,5000000,10000000) #c(10000,100000,500000,1000000,5000000,10000000)
-    sequencingDepthName<-c("10 k","100 k","500 k","1 M", "5 M", "10 M") #c("10k","100k","500k","1M", "5M", "10M")
+    sequencingDepth<-c(10000,100000,500000,1000000,5000000,10000000)
+    sequencingDepthName<-c("10 k","100 k","500 k","1 M", "5 M", "10 M")
     sequencingSaveName<-c("10k","100k","500k","1M", "5M", "10M")
   }
   
@@ -128,15 +114,17 @@ if (runStrata==T){
   DataStrata<-DESeq2_for_strata(Data,numberOfStrata)
 }
 #===================================================================================================================================
-## Setting upp the right environment and naming designs
+## Setting up the right environment and naming designs
 
+# Save-names for each design
 AllSaveDesigns=array(rep(NaN, length(effectsizes)*(length(groupSize)+extraL)*(length(sequencingDepth)+extraL)),c(length(effectsizes),(length(groupSize)+extraL),(length(sequencingDepth)+extraL))) 
+# Displayed name in plots for each design
 AllPlotDesigns=array(rep(NaN, length(effectsizes)*(length(groupSize)+extraL)*(length(sequencingDepth)+extraL)),c(length(effectsizes),(length(groupSize)+extraL),(length(sequencingDepth)+extraL))) 
 
 for (effect in 1:length(effectsizes)) {           # looping over q
   q=effectsizes[effect]
   
-  # Creating the standard designs
+  # Creating the main experimental designs
   for (group in 1:length(groupSize)){             # looping over m
     m=groupSize[group]
     for (seq in 1:length(sequencingDepth)) {      # looping over d
@@ -161,7 +149,7 @@ for (effect in 1:length(effectsizes)) {           # looping over q
     }
   }
   
-  # Creating 3 extra designs                               
+  # Creating 3 extra designs for trade-off analysis                               
   if (extraDesigns==T){
     for (i in 1:extraL) {              # looping over extra designs with fixed m and d
       m=extraGroups[i]
